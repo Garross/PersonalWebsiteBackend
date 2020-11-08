74415d7ead57839b6e2e6ca9670d5e7df25036a1
Garry Tiscovschi <tiscovsg@tcd.ie>
2020-11-08 11:10:58 +0000

    Changing boiler-plate API to Heroku Postgres

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ from flask_marshmallow import Marshmallow
 import os
 
 app = Flask(__name__)
+#Connect to Heroku Postgres Database
+app.config['Secret_Key'] = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get("DATABASE_URL")
 
-# database configuration
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'personal.db')
-
+#Initialize the database
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+db.init_app()
 
 @app.cli.command('db_create')
 def db_create():
@@ -30,16 +31,6 @@ def db_drop():
 
 @app.cli.command('db_seed')
 def db_seed():
-    mercury = Planet(planetName='Mercury',
-                     planetType='Class D',
-                     homeStar='Sol',
-                     mass=3.258e23)
-    earth = Planet(planetName='Earth',
-                   planetType='Class M',
-                   homeStar='Sol',
-                   mass=5.972e24)
-    db.session.add(mercury)
-    db.session.add(earth)
 
     test_user = User(firstName='William',
                      lastName='Herschel',
@@ -82,15 +73,26 @@ def url_variables(name: str, age: int):
         return jsonify(message="Welcome " + name + " you are old enough")
 
 
-@app.route('/planets')
-def planets():
-    planets_list = Planet.query.all()
-    result = planetsSchema.dump(planets_list)
-    return jsonify(result)
-    # Serializing is the process of converting an object into text
+@app.route('/ratings', method = ['get','post'] )
+def ratings():
+    if request.method == 'Post':
+        name = request.form['name']
+        score = request.form['score']
+
+        rating = Rating(name=name, score = score)
+
+        db.session.add(rating)
+        db.session.commit
+    elif request.method == 'Get':
+        ratingList=Rating.query.all()
+        # Serializing is the process of converting an object into text
+        result = ratingsSchema.dump(ratingList)
+        return jsonify(result)
 
 
-######
+
+
+#######
 # Models
 #######
 
@@ -104,31 +106,29 @@ class User(db.Model):
     password = Column(String)
 
 
-class Planet(db.Model):
-    __tablename__ = "planets"
-    planet_id = Column(Integer, primary_key=True)
-    planetName = Column(String)
-    planetType = Column(String)
-    homeStar = Column(String)
-    mass = Column(Float)
+class Rating(db.Model):
+    __tablename__ = "ratings"
+    score_id = Column(Integer, primary_key=True)
+    ratingName = Column(String)
+    ratingScore = Column(Float)
 
-
+#This is used for JSON serialization.
 class UserSchema(ma.Schema):
     class Meta:
         fields = ('user_id', 'firstName', 'lastName', 'email', 'password')
 
 
-#This is used for JSON serialization.
-class PlanetSchema(ma.Schema):
+
+class RatingSchema(ma.Schema):
     class Meta:
-        fields = ('planet_id', 'planetName', 'planetType', 'homeStar', 'mass')
+        fields = ('score_id', 'ratingName', 'ratingScore')
 
 
 userSchema = UserSchema
 usersSchema = UserSchema(many=True)
 
-planetSchema = PlanetSchema
-planetsSchema = PlanetSchema(many=True)
+ratingSchema = RatingSchema
+ratingsSchema = RatingSchema(many=True)
 
 # Entry point for running the script.
 if __name__ == '__main__':
